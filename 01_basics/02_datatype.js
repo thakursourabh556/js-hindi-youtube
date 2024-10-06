cc246b913665aa990e8532337d9af3a7fa8a6cf8
thakursourabh556 <143536368+thakursourabh556@users.noreply.github.com>
2024-10-06 03:05:35 +0000

    datatypes

---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,21 @@
+"use strict";  //treat all JS code as newer version
+//alert(3+3)  //we are using nodejs, not browser
+
+console.log( 3 + 3 )
+console.log("Sourabh")
+ 
+let name="sourabh"
+let age=20
+let isLoggedIn=false
+let state=null
+//number => 2 to power 53
+//bigint
+//string =>""
+//boolean =>true/false
+//undefined =>
+//null =>standalone value
+//symbol =>unique
+//object
+console.log(typeof age);    
+console.log(typeof null);  //object
+console.log(typeof undefined);  //undefined
