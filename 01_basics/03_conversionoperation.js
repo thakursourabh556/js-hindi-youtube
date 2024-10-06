6d090c699443efe398421ca00dfbb27d24ff2229
thakursourabh556 <143536368+thakursourabh556@users.noreply.github.com>
2024-10-06 03:32:07 +0000

    conversionofdatatype

---
+++ 01_basics/03_conversionoperation.js
@@ -0,0 +1,26 @@
+let score=true
+// console.log(typeof score);
+// console.log(typeof(score));
+ 
+let valueInNumber=Number(score);
+// console.log(typeof valueInNumber);  
+// console.log(valueInNumber);  
+
+// "33"  =>33
+// "33abc" =>NAN
+// TRUE =>1;
+//FALSE =>0
+  
+let isLoggedIn ="sourabh"
+
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);  
+
+// 1 =>true; 0=>false
+// "" =>false
+// "sourabh"  =>true
+
+let someNumber=33
+let stringNumber=String(someNumber)
+console.log(stringNumber);  
+console.log(typeof stringNumber);  
